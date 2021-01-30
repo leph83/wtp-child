d91679495db8a122953f1ca1478250100436b16b
leph83 <info@phuc.at>
2021-01-30 08:15:29 +0100

    optimized starting point

--- template-parts/footer/footer-nav.php
+++ template-parts/footer/footer-nav.php
@@ -10,11 +10,11 @@ wp_nav_menu(array(
     'container'     => '',
     'menu_class'    => 'nav  nav--inline  nav--footer',
     'fallback_cb'   => false,
-    'add_submenu_class'  => 'nav__submenu',
     'add_li_class'  => 'nav__item',
     'add_li_active_class' => 'nav__item--active',
     'add_li_parent_class' => 'nav__item--parent',
     'add_a_class'   => 'nav__link',
     'add_a_active_class'   => 'nav__link--active',
+    'add_submenu_class'  => 'nav__submenu',
 ));
 ?>
--- template-parts/header/site-navigation.php
+++ template-parts/header/site-navigation.php
@@ -13,14 +13,14 @@ if (!defined('ABSPATH')) {
         <?php
         wp_nav_menu(array(
             'theme_location'        => 'primary',
-            'menu_class'            => 'flex  flex-wrap--wrap  flex-justify--space-between  list-style-type--none  margin--0  padding--0',
+            'menu_class'            => 'nav  nav--primary  nav--header',
             'fallback_cb'           => false,
-            'add_submenu_class'     => '',
-            'add_li_class'          => '',
-            'add_li_active_class'   => '',
-            'add_li_parent_class'   => '',
-            'add_a_class'           => 'display--block  text-decoration--none  color--inherit  padding--half',
-            'add_a_active_class'    => 'color--2',
+            'add_li_class'          => 'nav__item',
+            'add_li_active_class'   => 'nav__item--active',
+            'add_li_parent_class'   => 'nav__item--parent',
+            'add_a_class'           => 'nav__link',
+            'add_a_active_class'    => 'nav__link--active',
+            'add_submenu_class'     => 'nav__submenu',
         ));
         ?>
     </nav>
