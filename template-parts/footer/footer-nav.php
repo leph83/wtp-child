924605a32684539056c2eeb1876b3d322e9fb35a
leph83 <info@phuc.at>
2021-02-03 09:39:12 +0100

    that's a pretty code - nobody

--- template-parts/footer/footer-nav.php
+++ template-parts/footer/footer-nav.php
@@ -6,15 +6,15 @@ if (!defined('ABSPATH')) {
 
 <?php
 wp_nav_menu(array(
-    'theme_location' => 'footer',
-    'container'     => '',
-    'menu_class'    => 'nav  nav--inline  nav--footer',
-    'fallback_cb'   => false,
-    'add_li_class'  => 'nav__item',
-    'add_li_active_class' => 'nav__item--active',
-    'add_li_parent_class' => 'nav__item--parent',
-    'add_a_class'   => 'nav__link',
-    'add_a_active_class'   => 'nav__link--active',
-    'add_submenu_class'  => 'nav__submenu',
+    'theme_location'        => 'footer',
+    'container'             => '',
+    'menu_class'            => 'nav  nav--inline  nav--footer',
+    'fallback_cb'           => false,
+    'add_li_class'          => 'nav__item',
+    'add_li_active_class'   => 'nav__item--active',
+    'add_li_parent_class'   => 'nav__item--parent',
+    'add_a_class'           => 'nav__link',
+    'add_a_active_class'    => 'nav__link--active',
+    'add_submenu_class'     => 'nav__submenu',
 ));
 ?>
