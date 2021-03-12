9c74246c1a84afe96bce1d6c154d761af3c532a9
leph83 <info@phuc.at>
2021-03-12 11:03:19 +0100

    phuctenberg it is

--- inc/load_css.php
+++ inc/load_css.php
@@ -17,13 +17,14 @@ if (!function_exists('wtp_load_styles_child')) {
             '06-components/component.footer.css',
 
             '06-components/component.nav.css',
-            '06-components/component.nav.mobile.css',
-            '06-components/component.nav.desktop.css',
 
             '06-components/component.wordpress.css',
-            '06-components/component.gutenberg.css',
-            '06-components/component.blocks.css',
-            '06-components/component.form.css',
+            '06-components/component.phuctenberg.css',
+            '06-components/component.gutenberg.custom.css',
+
+            //'06-components/component.gutenberg.css',
+            // '06-components/component.blocks.css',
+            // '06-components/component.form.css',
         );
 
         // STYLES
